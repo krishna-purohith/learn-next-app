d489134338ed6ee7b2b5e350610c7b2c06fadc50
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-05-08 13:41:41 +0530

    Done with Nextjs concepts

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+"use client";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+import { useState } from "react";
 
+export default function Home() {
   return (
     <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+      <h1>Hello World</h1>
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+          const sorted = _.orderBy(users, ["name"]);
+          const workers = [{ name: "k" }, { name: "l" }, { name: "o" }];
+          const sortedWorkers = _.orderBy(workers, ["name"]);
+          console.log(sorted);
+          console.log(sortedWorkers);
+        }}
+      >
+        Show
+      </button>
     </main>
   );
 }
